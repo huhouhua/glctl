bc142621a9554165e9e10177dd909f25b067cf1c
huhouhuam@outlook.com <huhouhuam@outlook.com>
2024-06-25 14:46:21 +0800

    add create branch test

--- cmd/resources/branch/create_branch_test.go
+++ cmd/resources/branch/create_branch_test.go
@@ -13,3 +13,101 @@
 // limitations under the License.
 
 package branch
+
+import (
+	"fmt"
+	"github.com/AlekSi/pointer"
+	cmdtesting "github.com/huhouhua/gitlab-repo-operator/cmd/testing"
+	cmdutil "github.com/huhouhua/gitlab-repo-operator/cmd/util"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/xanzy/go-gitlab"
+	"strings"
+	"testing"
+)
+
+func TestCreateBranch(t *testing.T) {
+	tests := []struct {
+		name        string
+		args        []string
+		optionsFunc func() *CreateOptions
+		validate    func(opt *CreateOptions, cmd *cobra.Command, args []string) error
+		run         func(opt *CreateOptions, args []string) error
+		wantError   error
+	}{{
+		name: "create a new branch",
+		args: []string{"create1"},
+		optionsFunc: func() *CreateOptions {
+			opt := NewCreateOptions()
+			opt.project = "huhouhua/gitlab-repo-branch"
+			opt.branch.Ref = pointer.ToString("main")
+			return opt
+		},
+		run: func(opt *CreateOptions, args []string) error {
+			var err error
+			out := cmdtesting.RunTestForStdout(func() {
+				err = opt.Run(args)
+			})
+			if !strings.Contains(out, *opt.branch.Branch) {
+				err = errors.New(fmt.Sprintf("delete by path : Unexpected output! Expected\n%s\ngot\n%s", *opt.branch.Branch, out))
+			}
+			return err
+		},
+		wantError: nil,
+	}, {
+		name: "create an existing branch",
+		args: []string{"create1"},
+		optionsFunc: func() *CreateOptions {
+			opt := NewCreateOptions()
+			opt.project = "huhouhua/gitlab-repo-branch"
+			return opt
+		},
+		run: func(opt *CreateOptions, args []string) error {
+			err := opt.Run(args)
+			var repo *gitlab.ErrorResponse
+			if errors.As(err, &repo) && repo.Message == "{error: ref is empty}" {
+				return nil
+			}
+			return err
+		},
+	}}
+	factory := cmdutil.NewFactory(cmdtesting.NewFakeRESTClientGetter())
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			cmd := NewCreateBranchCmd(factory)
+			var cmdOptions *CreateOptions
+			if tc.optionsFunc != nil {
+				cmdOptions = tc.optionsFunc()
+			} else {
+				cmdOptions = NewCreateOptions()
+			}
+			var err error
+			if err = cmdOptions.Complete(factory, cmd, tc.args); err != nil && !errors.Is(err, tc.wantError) {
+				t.Errorf("expected %v, got: '%v'", tc.wantError, err)
+				return
+			}
+			if tc.validate != nil {
+				err = tc.validate(cmdOptions, cmd, tc.args)
+				if err != nil {
+					return
+				}
+			} else {
+				if err = cmdOptions.Validate(cmd, tc.args); err != nil && !errors.Is(err, tc.wantError) {
+					t.Errorf("expected %v, got: '%v'", tc.wantError, err)
+					return
+				}
+			}
+			if tc.run != nil {
+				err = tc.run(cmdOptions, tc.args)
+				if err != nil {
+					t.Error(err)
+				}
+				return
+			}
+			if err = cmdOptions.Run(tc.args); !errors.Is(err, tc.wantError) {
+				t.Errorf("expected %v, got: '%v'", tc.wantError, err)
+				return
+			}
+		})
+	}
+}
