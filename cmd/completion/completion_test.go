1ae832ab20e39baafd39affc8991708141c0b6ab
huhouhuam@outlook.com <huhouhuam@outlook.com>
2024-07-13 23:54:03 +0800

    test: add test for completion

--- cmd/completion/completion_test.go
+++ cmd/completion/completion_test.go
@@ -13,3 +13,83 @@
 // limitations under the License.
 
 package completion
+
+import (
+	"github.com/huhouhua/glctl/util/cli"
+	"github.com/spf13/cobra"
+	"strings"
+	"testing"
+)
+
+func TestBashCompletions(t *testing.T) {
+	testCases := []struct {
+		name          string
+		args          []string
+		expectedError string
+	}{
+		{
+			name: "bash",
+			args: []string{"bash"},
+		},
+		{
+			name: "zsh",
+			args: []string{"zsh"},
+		},
+		{
+			name: "fish",
+			args: []string{"fish"},
+		},
+		{
+			name: "powershell",
+			args: []string{"powershell"},
+		},
+		{
+			name: "no args",
+			args: []string{},
+			expectedError: `Shell not specified.
+See 'glctl completion -h' for help and examples`,
+		},
+		{
+			name: "too many args",
+			args: []string{"bash", "zsh"},
+			expectedError: `Too many arguments. Expected only the shell type.
+See 'glctl completion -h' for help and examples`,
+		},
+		{
+			name: "unsupported shell",
+			args: []string{"foo"},
+			expectedError: `Unsupported shell type "foo".
+See 'glctl completion -h' for help and examples`,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(tt *testing.T) {
+			streams, _, out, _ := cli.NewTestIOStreams()
+			parentCmd := &cobra.Command{
+				Use: "glctl",
+			}
+			cmd := NewCmdCompletion(streams, defaultBoilerPlate)
+			parentCmd.AddCommand(cmd)
+			err := RunCompletion(out, defaultBoilerPlate, cmd, tc.args)
+			if tc.expectedError == "" {
+				if err != nil {
+					tt.Fatalf("Unexpected error: %v", err)
+				}
+				if out.Len() == 0 {
+					tt.Fatalf("Output was not written")
+				}
+				if !strings.Contains(out.String(), defaultBoilerPlate) {
+					tt.Fatalf("Output does not contain boilerplate:\n%s", out.String())
+				}
+			} else {
+				if err == nil {
+					tt.Fatalf("An error was expected but no error was returned")
+				}
+				if err.Error() != tc.expectedError {
+					tt.Fatalf("Unexpected error: %v\nexpected: %v\n", err, tc.expectedError)
+				}
+			}
+		})
+	}
+}
